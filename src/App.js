20470dcc5e18b4366a3f7c32de51dea2918c4ab3
Omar <>
2021-03-16 13:52:07 -0500

    iniciando react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React en menos de un dia
         </a>
       </header>
     </div>
